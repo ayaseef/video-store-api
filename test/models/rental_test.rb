00ec2f76381d1bdac0fc5ca2668d236da3e22c02
beauttie <bak02013@pomona.edu>
2020-12-04 00:10:03 -0800

    Finish Rental model tests and passing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.create(customer: customers(:customer_one), video: videos(:black_widow)) }
+  let(:rental) {
+    Rental.new(customer: customers(:customer_one),
+               video: videos(:black_widow))
+  }
 
   describe "validations" do
     it 'is valid when required fields are present' do
@@ -21,7 +24,8 @@ describe Rental do
       expect(result).must_equal false
     end
   end
-  describe "relationaships" do
+
+  describe "relations" do
     it "belongs to customer" do
       rental.must_respond_to :customer
       (rental.customer).must_be_kind_of Customer
@@ -33,5 +37,60 @@ describe Rental do
     end
   end
 
+  describe "custom methods" do
+    describe "check_out_video" do
+      it "saves a valid rental to db" do
+        rental.check_out_video
+
+        expect(rental.save).must_equal true
+      end
+
+      it "will decrease the available_inventory of video by 1" do
+        before_inventory = rental.video.available_inventory
+        rental.check_out_video
+
+        expect(rental.video.available_inventory).must_equal before_inventory - 1
+      end
+
+      it "will increase videos_checked_out_count for customer by 1" do
+        before_count = rental.customer.videos_checked_out_count
+        rental.check_out_video
+
+        expect(rental.customer.videos_checked_out_count).must_equal before_count + 1
+      end
+
+      it "will not save rental to db when available_inventory is equal to 0" do
+        rental.video.available_inventory = 0
+        expect(rental.check_out_video).must_equal false
+      end
+    end
+
+    describe "check_in_video" do
+      it "saves a valid rental to db" do
+        rental.check_in_video
+
+        expect(rental.save).must_equal true
+      end
+
+      it "will increase the available_inventory of video by 1" do
+        before_inventory = rental.video.available_inventory
+        rental.check_in_video
+
+        expect(rental.video.available_inventory).must_equal before_inventory + 1
+      end
+
+      it "will decrease videos_checked_out_count for customer by 1" do
+        before_count = rental.customer.videos_checked_out_count
+        rental.check_in_video
+
+        expect(rental.customer.videos_checked_out_count).must_equal before_count - 1
+      end
+
+      it "will not save rental to db when videos_checked_out_count is equal to 0" do
+        rental.customer.videos_checked_out_count = 0
+        expect(rental.check_in_video).must_equal false
+      end
+    end
+  end
 end
 
