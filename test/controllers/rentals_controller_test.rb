90c22ef25f4647279204d54a79e66c5e81ab10cd
beauttie <bak02013@pomona.edu>
2020-12-04 01:36:52 -0800

    Finish Rental controller tests and passing

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,6 +1,15 @@
 require "test_helper"
 
 describe RentalsController do
+  def check_response(expected_type:, expected_status: :ok)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
   before do
     @customer = customers(:customer_one)
     @video = videos(:wonder_woman)
@@ -17,12 +26,10 @@ describe RentalsController do
         post checkout_path, params: @request_body
       }.must_change "Rental.count", 1
 
-      # does a find_by
-      @customer.reload
+      body = check_response(expected_type: Hash)
 
-      must_respond_with :ok
-
-      expect(@customer.videos_checked_out_count).must_equal 4
+      fields = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
+      expect(body.keys.sort).must_equal fields
     end
 
     it "cannot check out a video if the customer does not exist and returns not found" do
@@ -32,7 +39,9 @@ describe RentalsController do
         post checkout_path, params: @request_body
       }.wont_change "Rental.count"
 
-      must_respond_with :not_found
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
     end
 
     it "cannot check out a video if the video does not exist and returns not found" do
@@ -42,7 +51,9 @@ describe RentalsController do
         post checkout_path, params: @request_body
       }.wont_change "Rental.count"
 
-      must_respond_with :not_found
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
     end
 
     it "cannot check out a video if the video does not have any available inventory and returns bad request" do
@@ -53,58 +64,67 @@ describe RentalsController do
         post checkout_path, params: @request_body
       }.wont_change "Rental.count"
 
-      must_respond_with :bad_request
+      body = check_response(expected_type: Hash, expected_status: :bad_request)
+      expect(body.keys).must_include "errors"
     end
   end
 
   describe "check_in" do
-    # use reload when checking before and after counts
     it "can check in a video for a customer" do
       post checkout_path, params: @request_body
-      rental = Rental.find_by(customer_id: @request_body[:customer_id], video_id: @request_body[:video_id])
-      expect(rental).wont_be_nil
 
       expect {
         post checkin_path, params: @request_body
       }.wont_change "Rental.count"
 
-      # does a find_by
-      @customer.reload
+      body = check_response(expected_type: Hash)
 
-      must_respond_with :ok
+      fields = ["customer_id", "video_id", "videos_checked_out_count", "available_inventory"].sort
+      expect(body.keys.sort).must_equal fields
+    end
 
-      expect(@customer.videos_checked_out_count).must_equal 3
+    it "cannot check in a video if rental for that video does not exist" do
+      post checkin_path, params: @request_body
+
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
     end
 
     it "cannot check out a video if the customer does not exist and returns not found" do
-      # @request_body[:customer_id] = nil
-      #
-      # expect {
-      #   post checkout_path, params: @request_body
-      # }.wont_change "Rental.count"
-      #
-      # must_respond_with :not_found
+      post checkout_path, params: @request_body
+      @request_body[:customer_id] = nil
+
+      post checkin_path, params: @request_body
+
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
     end
 
     it "cannot check out a video if the video does not exist and returns not found" do
-      # @request_body[:video_id] = nil
-      #
-      # expect {
-      #   post checkout_path, params: @request_body
-      # }.wont_change "Rental.count"
-      #
-      # must_respond_with :not_found
+      post checkout_path, params: @request_body
+      @request_body[:video_id] = nil
+
+      post checkin_path, params: @request_body
+
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
     end
 
-    it "cannot check out a video if the video does not have any available inventory and returns bad request" do
-      # @video.available_inventory = 0
-      # @video.save!
-      #
-      # expect {
-      #   post checkout_path, params: @request_body
-      # }.wont_change "Rental.count"
-      #
-      # must_respond_with :bad_request
+    it "cannot check in a video if the customer has no videos checked out and returns bad request" do
+      post checkout_path, params: @request_body
+      @customer.videos_checked_out_count = 0
+      @customer.save!
+
+      post checkin_path, params: @request_body
+
+      body = check_response(expected_type: Hash, expected_status: :bad_request)
+      expect(body.keys).must_include "errors"
     end
   end
 end
